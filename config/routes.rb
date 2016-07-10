a95f957fdb9add62a4ec0161fabdf03120b59714
choyt800 <choyt800@gmail.com>
2016-07-10 05:58:41 +0000

    fixed background image

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :memberships
   resources :members
   resources :keycards
+  resources :charges
   get '/inactive' => 'members#inactive'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -58,6 +59,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  resources :charges
-  
+
 end
