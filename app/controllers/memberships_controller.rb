188be8da1b6f14b04901845e28ce16a6e2c94f52
choyt800 <choyt800@gmail.com>
2016-08-23 00:05:38 +0000

    added order back to memberships

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,7 +4,7 @@ class MembershipsController < ApplicationController
   # GET /memberships
   # GET /memberships.json
   def index
-    @memberships = Membership.all
+    @memberships = Membership.all.order('lower(paid_by) ASC')
   end
 
   # GET /memberships/1
