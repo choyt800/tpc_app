48023013cd2acbd9c951ceebe81c98266a402687
David Ker <snappy316@gmail.com>
2017-11-01 15:18:19 -0700

    Support and Display New Plan Categories
    We've added two new Plan Categories: "Resident Access", and "Community
    Access". This commit adds these categories to the scope used to display
    plans on the new / edit Membership form.

--- app/models/plan_category.rb
+++ app/models/plan_category.rb
@@ -1,7 +1,7 @@
 class PlanCategory < ActiveRecord::Base
   has_many :plans
 
-  scope :memberships, -> { where('name IN (?)', ['Designated Desk', 'Communal Membership', 'Team Membership', 'Non-Stripe']) }
+  scope :memberships, -> { where('name IN (?)', ['Resident Access', 'Community Access', 'Designated Desk', 'Communal Membership', 'Team Membership', 'Non-Stripe']) }
   scope :mail_service, -> { where(name: 'Mail Service') }
   scope :misc, -> { where(name: 'Misc') }
   scope :uncategorized, -> { Plan.all.where(plan_category_id: nil) }
