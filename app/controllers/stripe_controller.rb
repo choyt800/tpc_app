040467afb4667a342c5e000ba687ce447ed562cf
David Ker <snappy316@gmail.com>
2018-05-28 16:19:37 -0700

    Update Stripe Webhooks
    We'll only look for Custom Subscriptions now.

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -11,17 +11,17 @@ class StripeController < ApplicationController
       event_period_end = event_data[:period_end]
       event_amount_due = event_data[:amount_due]
 
-      membership = Membership.find_by(stripe_sub_id: event_subscription)
+      custom_sub = CustomSubscription.find_by(stripe_sub_id: event_subscription)
       mail_service = MailService.find_by(stripe_sub_id: event_subscription)
 
-      if membership
-        membership.update_attributes!(next_invoice_date: Time.at(event_period_end), invoice_amount: event_amount_due)
-        puts "Updated membership #{membership.id} with Next Invoice Date of #{Time.at(event_period_end)} and Invoice Amount of #{event_amount_due}"
+      if custom_sub
+        custom_sub.update_attributes!(next_invoice_date: Time.at(event_period_end), invoice_amount: event_amount_due)
+        puts "Updated custom subscription #{custom_sub.id} with Next Invoice Date of #{Time.at(event_period_end)} and Invoice Amount of #{event_amount_due}"
       elsif mail_service
         mail_service.update_attributes!(next_invoice_date: Time.at(event_period_end), invoice_amount: event_amount_due)
         puts "Updated mail service #{mail_service.id} with Next Invoice Date of #{Time.at(event_period_end)} and Invoice Amount of #{event_amount_due}"
       else
-        puts "No membership or mail service found with Stripe Sub ID #{event_subscription}"
+        puts "No custom subscription or mail service found with Stripe Sub ID #{event_subscription}"
       end
     end
 
