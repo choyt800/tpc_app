d0633b8c057a8301772da2df3af47d207f6d3e23
David Ker <snappy316@gmail.com>
2018-07-21 07:57:31 -0700

    Change Intervals
    Change the `days_away` interval to 30, and don't send to subs with a 1 
    month interval.

--- app/workers/check_renewal_window_worker.rb
+++ app/workers/check_renewal_window_worker.rb
@@ -6,13 +6,11 @@ class CheckRenewalWindowWorker
       if cs.stripe_sub_id.present?
         days_away = (cs.next_invoice_date.to_date - Date.tomorrow.to_date).to_i
 
-        # if days_away == 45
-        if days_away == -13
+        if days_away == 30
           stripe_sub = Stripe::Subscription.retrieve(cs.stripe_sub_id)
           interval = "#{stripe_sub.items.data.first.plan.interval_count} #{stripe_sub.items.data.first.plan.interval}"
 
-          # if interval == '6 month'
-          SendRenewalEmailWorker.perform_async(cs.id) if interval == '1 month'
+          SendRenewalEmailWorker.perform_async(cs.id) unless interval == '1 month'
         end
       end
     end
